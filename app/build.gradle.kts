8671b9416398c9d58779a98270c4402a05b888e8
LeshLiao <dlnx38@gmail.com>
2025-03-10 02:03:47 +0800

    - Release 2.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.palettex.palettewall"
         minSdk = 30
         targetSdk = 34
-        versionCode = 216
-        versionName = "2.16"
+        versionCode = 217
+        versionName = "2.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
